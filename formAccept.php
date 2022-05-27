94b506746b5ace9ac7c2c0b24e6c827928a3e9e8
DawidKraszkiewicz <39964989+DawidKraszkiewicz@users.noreply.github.com>
2022-05-27 14:51:11 +0200

    Update formAccept.php

--- formAccept.php
+++ formAccept.php
@@ -58,8 +58,8 @@
 //		$mail->isSMTP();                                            
 //		$mail->Host       = 'smtp.gmail.com;';                    
 //		$mail->SMTPAuth   = true;                             
-//		$mail->Username   = 'd.kraszkiewicz2002@gmail.com';                 
-//		$mail->Password   = 'Kraszka19741!';                        
+//		$mail->Username   = '';                 
+//		$mail->Password   = '';                        
 //		$mail->SMTPSecure = 'tls';                              
 //		$mail->Port       = 587;  
 //		
